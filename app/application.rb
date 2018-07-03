90cf32a074cd4ad021ebf7e20ff504db8cdde081
Veronica Bustabad <vbustabad@gmail.com>
2018-07-03 18:38:00 +0000

    automatically backed up by learn

--- app/application.rb
+++ app/application.rb
@@ -17,12 +17,12 @@ class Application
     elsif req.path.match(/cart/)
 
       if @@cart.empty?
-        puts "Your cart is empty"
-      else 
+        resp.write "Your cart is empty"
+      else
         @@cart.each do |item|
           resp.write "#{item}\n"
         end
-      end 
+      end
     elsif req.path.match(/add/)
       add_item = req.params["item"]
 
